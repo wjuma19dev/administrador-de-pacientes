694e505a741ac8fc1791d4724a0bc47354ea945c
Wilson <wjuma19dev@gmail.com>
2025-03-11 17:04:47 -0400

    Trabajando con los archivos javascript y el formulario

---
+++ js/formulario.js
@@ -0,0 +1,13 @@
+const CITAS_FORM = document.querySelector("#nueva-cita-form");
+
+const nuevaCita = {
+  paciente: "",
+  edad: "",
+  seguro: "",
+  dir: "",
+  cuadro: "",
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(CITAS_FORM);
+});
---
+++ js/models/cita.js
@@ -0,0 +1,11 @@
+class Cita {
+  constructor(paciente, edad, seguro, dir, cuadro) {
+    this.paciente = paciente;
+    this.edad = edad;
+    this.seguro = seguro;
+    this.dir = dir;
+    this.cuadro = cuadro;
+  }
+}
+
+export default Cita;
