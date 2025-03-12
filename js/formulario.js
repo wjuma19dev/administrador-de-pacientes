f4275477cee0ac9d28674e272d4ea9b71d2322d3
wilson <wjuma19dev@gamil.com>
2025-03-11 23:40:40 -0700

    Agregando ligera validacion al formulario

--- js/formulario.js
+++ js/formulario.js
@@ -30,6 +30,15 @@ document.addEventListener("DOMContentLoaded", () => {
   // TODO: Enviar Formulario aqui
   CITAS_FORM.addEventListener("submit", function (e) {
     e.preventDefault();
+
+    const isValid = Object.values(nuevaCita).some(
+      (campo) => campo.trim().length === 0
+    );
+    if (!!isValid) {
+      console.log("Todos los campos son obligatorios");
+      return;
+    }
+
     const cita = new Cita(nuevaCita);
     cita.create().then((citaCreada) => console.log(citaCreada));
   });
